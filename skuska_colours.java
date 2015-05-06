3d6e1cc86b42df825b7392bd89369b8b58074be1
durajkaDusan <durajkadusan@gmail.com>
2015-05-06 09:58:16 +0200

    222

--- skuska_colours.java
+++ skuska_colours.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Red");
+        System.out.println("Red2");
 	System.out.println("Blue")
     }
 
