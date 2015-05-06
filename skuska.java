9bd3c15b38f339538d462c1bfa0731cb7093afb8
durajkaDusan <durajkadusan@gmail.com>
2015-05-06 09:58:57 +0200

    5

--- skuska.java
+++ skuska.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
-	System.out.println("Hello, World Second time")
+	System.out.println("Hello, World Secon5d time")
     }
 
 }
